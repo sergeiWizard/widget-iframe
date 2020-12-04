4ceb97171b88b16cea9d92b01cdeb43f763d66b3
Sergei <sergeij@wizardsdev.com>
2020-12-04 10:24:51 +0200

    styls

--- src/WidgetAll.js
+++ src/WidgetAll.js
@@ -5,8 +5,8 @@ const WidgetAll = () => {
         <iframe
             className="waivio"
             src={`http://localhost:3000/rewards/all?display=widget&userName=vallon&ref=vallon`}
-            height="1000"
-            width="1000"
+            height="800"
+            width="750"
             frameBorder="0"
         >
             Can't load Rewards widget.
